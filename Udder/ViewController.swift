b911c1abc052f646cdf2232c32615b4a52d0234e
Zvonko Nikolich <lureofthesea@gmail.com>
2018-09-05 14:08:32 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Zvonko on 9/5/18.
+//  Copyright © 2018 ZvoNik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
